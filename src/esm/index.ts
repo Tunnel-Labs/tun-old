8e4d79355321b01ede330b69d444fe7d3073f9c6
Leon Si <leon@leondreamed.com>
2023-11-16 11:07:42 -0500

    check that file exists

--- src/esm/index.ts
+++ src/esm/index.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import { createRequire } from 'node:module';
 import { isMainThread } from 'node:worker_threads';
@@ -10,7 +11,11 @@ if (supportsModuleRegister && isMainThread) {
 	// ESM, which breaks for extension-less JavaScript files.
 	// Thus, if we detect that the entrypoint is an extension-less file, we
 	// short-circuit and load it via CommonJS instead.
-	if (process.argv[1] !== undefined && path.extname(process.argv[1]) === '') {
+	if (
+		process.argv[1] !== undefined &&
+		path.extname(process.argv[1]) === '' &&
+		fs.existsSync(process.argv[1])
+	) {
 		createRequire(import.meta.url)(process.argv[1]);
 	} else {
 		registerLoader();
